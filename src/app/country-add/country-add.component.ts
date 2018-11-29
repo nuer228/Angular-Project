636c911ccad06bb525d7e06d11c852caa20dbcab
Kristaps Rutkovskis <35775381+nuer228@users.noreply.github.com>
2018-11-29 18:29:51 +0000

    Added Send

--- src/app/country-add/country-add.component.ts
+++ src/app/country-add/country-add.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
-import {PostService} from '../services/post.service';
+import {SendService} from '../services/send.service';
 import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
 import { Observable } from 'rxjs';
-import {Post} from '../post.model';
+import {Send} from '../send.model';
 import {Router, ActivatedRoute} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MAT_STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
@@ -27,12 +27,12 @@ export class CountryAddComponent implements OnInit {
   secondFormGroup: FormGroup;
   ThirdFormGroup: FormGroup;
 
-  constructor(private service:PostService) { }
+  constructor(private service:SendService) { }
 
   // Allows us to add fields to MangoDB
-  onAddPost(form: NgForm) {
+  onAddSend(form: NgForm) {
 
-    this.service.addPost(form.value.country, form.value.capital, form.value.population).subscribe();
+    this.service.addSend(form.value.country, form.value.capital, form.value.population).subscribe();
     
     console.log(form.value);
     form.resetForm();
