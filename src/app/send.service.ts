a529bb2c89980426dae637642d78c0a30b1cf56f
Kristaps Rutkovskis <35775381+nuer228@users.noreply.github.com>
2018-11-29 18:33:10 +0000

    Added Send

---
+++ src/app/send.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Send} from '../send.model';
+//import { Stream } from 'stream';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+// Send Service to connect our CRUD to MangoDB.
+export class SendService {
+
+  constructor(private http: HttpClient) { }
+  
+    getSendData(): Observable<any> {
+      return this.http.get("http://localhost:8081/api/send");
+    }
+
+  addSend(country: string, capital: string, population: string): Observable<any> {
+    const send: Send = {country: country, capital: capital, population: population};
+    return this.http.post("http://localhost:8081/api/send",send);
+  }
+
+  deleteSend(id: String): Observable<any> {
+    return this.http.delete("http://localhost:8081/api/send/"+id);
+  }
+
+  getSend(id:String): Observable<any> {
+    return this.http.get("http://localhost:8081/api/send/"+id);
+  }
+
+  updateSend(id:String, country: string, capital: string, population: string): Observable<any> {
+    const send: Send = {country: country, capital: capital, population: population};
+  return this.http.put("http://localhost:8081/api/send/"+id, send);
+  }
+}
